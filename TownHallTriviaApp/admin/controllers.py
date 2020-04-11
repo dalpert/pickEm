0051c6a4122b3d7f23e49cd42259d0964543487c
Adam Lassman <adam.lassman@gmail.com>
2020-04-11 16:33:11 -0700

    added local sessions

--- TownHallTriviaApp/admin/controllers.py
+++ TownHallTriviaApp/admin/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 
 
 admin = Blueprint("admin", __name__, template_folder="templates")
@@ -6,4 +6,4 @@ admin = Blueprint("admin", __name__, template_folder="templates")
 
 @admin.route('/')
 def home():
-    return render_template('admin/home.html')
+    return render_template('admin/home.html', session=session)
